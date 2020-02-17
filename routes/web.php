197f6353fd6f2984a8ad19d2e76f6a4933b9692d
Thiago Mendes <thiagomendeswd@gmail.com>
2020-02-17 18:18:34 -0300

    paginas de login e registro

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,10 @@ Route::get('/', 'Site\HomeController@index');
 
 ROute::prefix('painel')->group(function(){
     Route::get('/', 'Admin\HomeController@index')->name('admin');
+
     Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
+    Route::post('login', 'Admin\Auth\LoginCOntroller@authenticate');
+
+    Route::get('register', 'Admin\Auth\RegisterController@index')->name('registr');
+    Route::post('register', 'Admin\Auth\RegisterController@register');
 });
