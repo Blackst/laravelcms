03058f191d26583ba83d07c3e9188fe72269624f
Thiago Mendes <thiagomendeswd@gmail.com>
2020-02-17 21:02:07 -0300

    Função de login

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -43,12 +45,41 @@ class LoginController extends Controller
         return view('admin.login');
     }
 
-    public function authenticate(){
-        
+    public function authenticate(Request $request){
+        $data = $request->only([
+            'email',
+            'password',
+            'remember'
+        ]);
+
+        $validator = $this->validator($data);
+
+        if ($validator->fails()) {
+            return redirect()->route('login')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        if (Auth::attempt($data)) {
+            return redirect()->route('admin');
+        }else{
+            $validator->errors()->add('password', 'E-mail e/ou senha errados!');
+
+            return redirect()->route('login')
+            ->withErrors($validator)
+            ->withInput();
+        }
     }
 
     public function logout(){
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    public function validator(array $data){
+        return Validator::make($data, [
+            'email'=> ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:4']
+        ]);
     }
 }
