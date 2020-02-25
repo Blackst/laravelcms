4c05575d6f1cb3e2d8ca47f5e1a384d00ac08a64
Thiago Mendes <thiagomendeswd@gmail.com>
2020-02-25 17:24:29 -0300

    Informações de configuração

--- resources/views/admin/Settings/index.blade.php
+++ resources/views/admin/Settings/index.blade.php
@@ -16,35 +16,35 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Título do Site</label>
                     <div class="col-sm-10">
-                        <input type="text" name="title" value="" class="form-control">
+                    <input type="text" name="title" value="{{$settings['title']}}" class="form-control">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Sub-título do Site</label>
                     <div class="col-sm-10">
-                        <input type="text" name="subtitle" value="" class="form-control">
+                        <input type="text" name="subtitle" value="{{$settings['subtitle']}}" class="form-control">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">E-mail para contato</label>
                     <div class="col-sm-10">
-                        <input type="email" name="email" value="" class="form-control">
+                        <input type="email" name="email" value="{{$settings['email']}}" class="form-control">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Cor do fundo</label>
                     <div class="col-sm-10">
-                        <input type="color" name="bgcolor" value="" class="form-control">
+                        <input type="color" name="bgcolor" value="{{$settings['bgcolor']}}" class="form-control">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Cor do texto</label>
                     <div class="col-sm-10">
-                        <input type="color" name="textcolor" value="" class="form-control">
+                        <input type="color" name="textcolor" value="{{$settings['textcolor']}}" class="form-control">
                     </div>
                 </div>
 
